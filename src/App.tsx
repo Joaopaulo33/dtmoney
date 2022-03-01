04254cb8c159a9d0b0861c8b0cefd60d55b86a5a
João paulo C <joaopaulo33573@gmail.com>
2022-03-01 09:31:41 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
 
-function App() {
+// const Title = styled.h1`
+//   font-size: 64px;
+//   color:#8257e6;
+//   `
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+     <GlobalStyle/>
+     
+    </>
   );
 }
 
-export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+    :root{
+        --background:#f0f2f5;
+        --red:#E52E4D;
+        --blue:#5429CC;
+
+        --blue-light:#6933FF;
+
+        --text-title:#363F5F;
+        --text-body:#969CB3;
+
+        --shape:#FFFFFF;
+
+    }
+
+    *{
+        margin:0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+ /* quando usuário estiver com uma tela até 1080px de largura vamos diminuir o fontsiz para 93.75%
+    e quando tiver em uma meno que 720px vai diminuir pra 87.5 porcento */
+    html{
+    @media(max-width:1080px){
+        font-size:93.75%;
+    }
+    @media(max-width:720px){
+        font-size:87.5%;
+    }
+
+
+    body{
+        background: (--background);
+        -webkit-font-smooting: antialiased;
+        }
+    
+    /* Não pode colocar só no corpo porque os outros não importam automaticamente a fonte do corpo do htmel */
+    body, input,textarea,button{
+
+        font-family: 'Poppins',sans-serif;
+        font-weight: 400;
+        
+    }
+
+    h1,h2,h3,h4,h4,h6,strong{
+        font-weight: 600;
+    }
+    
+    button{
+        cursor: pointer;
+    }
+
+    [disabled]{
+        opacity: 0.6;
+        cursor:not-allowed;
+    }
+   
+}
+`   
