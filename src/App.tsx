24af7bbc4607fc6794f0afa9898307d197914770
João paulo C <joaopaulo33573@gmail.com>
2022-03-02 16:23:08 -0300

    configurando modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,37 @@ import styled from 'styled-components'
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import Modal from 'react-modal';
+import { useState } from 'react';
+
+
 
 // const Title = styled.h1`
 //   font-size: 64px;
 //   color:#8257e6;
 //   `
+
+
 export function App() {
+
+  const[isNewTrasactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function  handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+}
+
+function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+}
+
   return (
     <>
-    <Header/>
+    <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
     <Dashboard/>
+    <Modal 
+      isOpen={isNewTrasactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}>
+    </Modal>
     <GlobalStyle/>
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,17 @@
-import logoImg from '../../assets/logo.svg'
-import { Container, Content } from './styles'
-export function Header(){
+import { useState } from 'react';
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+
+interface HeaderProps{
+    onOpenNewTransactionModal:()=>void;
+}
+export function Header({onOpenNewTransactionModal}:HeaderProps){
+   
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Content>
